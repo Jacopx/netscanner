96a7e9c7bfc60e5ce524db9f2eda14d1bd3c083b
Jacopx <jacopo.nasi@gmail.com>
2016-12-09 23:56:20 +0100

    Adding vendor [View problem]

--- netscan.py
+++ netscan.py
@@ -2,11 +2,36 @@ import nmap
 import datetime
 import time
 
-# addr = raw_input('Insert the host to be scanned: ')
-# netmask = raw_input('Insert the netmask (prefix length): ')
+# Network scanner function return the 2 list of MAC and IP addresses
+def scanner(nm, sadd = '192.168.1.0/24'):
+    mac = []
+    ip = []
+    vend = []
+    nm.scan(hosts=sadd, arguments='-sP')
+    for h in nm.all_hosts():
+        if 'mac' in nm[h]['addresses']:
+            mac.append(nm[h]['addresses']['mac'])
+            ip.append(nm[h]['addresses']['ipv4'])
+            if nm[h]['vendor'].iteritems()>0:
+                vend.append(nm[h]['vendor'].values())
+            else:
+                vend.append('NOT RETRIVE')
+    return ip, mac, vend
 
+# Header of the software with time
+now = datetime.datetime.now() #get time
+print '\nNetwork Scanner by Jacopx -- ' + (now.strftime("%d/%m/%Y %H:%M"))
+print '---------------------------------------------'
+
+# Network Informations
+sc_add = raw_input('Address/Netmask(PL): ')
 nm = nmap.PortScanner()
-nm.scan(hosts='192.168.1.0/24', arguments='-sL')
-for h in nm.all_hosts():
-    if 'mac' in nm[h]['addresses']:
-        print(nm[h]['addresses'], nm[h]['vendor'])
+
+# Call scan function
+if len(sc_add) == 0:
+    ip, mac, vend = scanner(nm) # Function call for scan default network 192.168.1.0/24
+else:
+    ip, mac, vend = scanner(nm, sc_add) # Function call for scan network host insert by user
+
+for i in range(0,len(mac)):
+    print '%s\t%s\t%s' %(ip[i], mac[i], vend[i])
