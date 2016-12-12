11ba3be01d5f9106d572b6ab7eb0aa83d7bf9556
Jacopx <jacopo.nasi@gmail.com>
2016-12-12 23:29:25 +0100

    Display correctly everything

--- netscan.py
+++ netscan.py
@@ -6,14 +6,36 @@ import time
 def scanner(nm, sadd = '192.168.1.0/24'):
     mac = []
     ip = []
-    vend = []
+    vend = {}
     nm.scan(hosts=sadd, arguments='-sP')
     for h in nm.all_hosts():
         if 'mac' in nm[h]['addresses']:
             mac.append(nm[h]['addresses']['mac'])
             ip.append(nm[h]['addresses']['ipv4'])
-            vend.append(nm[h]['vendor'].values())
+            vend.update(nm[h]['vendor'])
     return ip, mac, vend
+
+# Function for printing the net table
+def printer(ip, mac, vend):
+    i=0
+    print '-------------------------------------------------------------'
+    print '|      IP      |      MAC addrs      |        Vendor        |'
+    print '-------------------------------------------------------------'
+    for k in mac:
+        if k in vend:
+            print ' %s\t%s\t%s' % (ip[i], k, vend[k])
+        else:
+            print ' %s\t%s' % (ip[i], k)
+        i=i+1
+# Function for printing the net table
+def fprinter(f, ip, mac, vend):
+    i=0
+    for k in mac:
+        if k in vend:
+            f.write(ip[i] + '  ' + k + '  ' + vend[k] + '\n')
+        else:
+            f.write(ip[i] + '  ' + k + '\n')
+        i=i+1
 
 # Header of the software with time
 now = datetime.datetime.now() #get time
@@ -29,6 +51,6 @@ if len(sc_add) == 0:
     ip, mac, vend = scanner(nm) # Function call for scan default network 192.168.1.0/24
 else:
     ip, mac, vend = scanner(nm, sc_add) # Function call for scan network host insert by user
-
-for i in range(0,len(mac)):
-    print '%s\t%s\t%s' %(ip[i], mac[i], vend[i])
+f = open('database.txt', 'w')
+printer(ip,mac,vend) # Calling print function
+fprinter(f,ip,mac,vend) # Calling print on file function
