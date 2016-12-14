2f20d85c5cba18be446b77bf3e507bf8b5372660
Jacopx <jacopo.nasi@gmail.com>
2016-12-14 11:55:54 +0100

    Menu added

--- netscan.py
+++ netscan.py
@@ -1,6 +1,7 @@
 import nmap
 import datetime
 import time
+import os
 
 # Network scanner function return the 2 list of MAC and IP addresses
 def scanner(nm, sadd = '192.168.1.0/24'):
@@ -23,34 +24,58 @@ def printer(ip, mac, vend):
     print '-------------------------------------------------------------'
     for k in mac:
         if k in vend:
-            print ' %s\t%s\t%s' % (ip[i], k, vend[k])
+            print ' %s\t %s\t%s' % (ip[i], k, vend[k])
         else:
-            print ' %s\t%s' % (ip[i], k)
+            print ' %s\t %s' % (ip[i], k)
         i=i+1
 # Function for printing the net table
 def fprinter(f, ip, mac, vend):
     i=0
     for k in mac:
         if k in vend:
-            f.write(ip[i] + '  ' + k + '  ' + vend[k] + '\n')
+            f.write(ip[i] + ',' + k + ',' + vend[k] + '\n')
         else:
-            f.write(ip[i] + '  ' + k + '\n')
+            f.write(ip[i] + ',' + k + '\n')
         i=i+1
 
-# Header of the software with time
-now = datetime.datetime.now() #get time
-print '\nNetwork Scanner by Jacopx -- ' + (now.strftime("%d/%m/%Y %H:%M"))
-print '---------------------------------------------'
+# START SCRIPT WITH MENU
+c=0
+while c!=9:
+    now = datetime.datetime.now() #get time
+    print '\nNetwork Scanner by Jacopx -- ' + (now.strftime("%d/%m/%Y %H:%M"))
+    print '---------------------------------------------'
+    print '1. Simple Network Scan'
+    print '2. Net Database Comparison'
+    print '3. Showing Database'
+    print '4. Edit Database'
+    print '5. Clear Database'
+    print '9. EXIT'
+    c=input('Choose: ')
+    print '---------------------------------------------'
 
-# Network Informations
-sc_add = raw_input('Address/Netmask(PL): ')
-nm = nmap.PortScanner()
+    if c==1: # Simple Network Scan
+        # Network Informations
+        sc_add = raw_input('Address/Netmask(PL): ')
+        nm = nmap.PortScanner()
+        # Call scan function
+        if len(sc_add) == 0:
+            ip, mac, vend = scanner(nm) # Function call for scan default network 192.168.1.0/24
+        else:
+            ip, mac, vend = scanner(nm, sc_add) # Function call for scan network host insert by user
+        printer(ip,mac,vend) # Calling print function
+        c=input('Back to home (1): ')
+        if c==1:
+            os.system('clear')
+            continue
+        else:
+            raise SystemExit
+    elif c==2:
+        print 'Current Network: ' + os.system('nm-tool | grep \*')
+    # elif c==3:
+    #
+    # elif c==4:
 
-# Call scan function
-if len(sc_add) == 0:
-    ip, mac, vend = scanner(nm) # Function call for scan default network 192.168.1.0/24
-else:
-    ip, mac, vend = scanner(nm, sc_add) # Function call for scan network host insert by user
-f = open('database.txt', 'w')
-printer(ip,mac,vend) # Calling print function
-fprinter(f,ip,mac,vend) # Calling print on file function
+    elif c==9:
+        print 'Goodbye!'
+        raise SystemExit
+    print '---------------------------------------------'
