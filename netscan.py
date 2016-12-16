9d08934af8d52f7971113b885dbe99cdd947e7e3
Jacopx <jacopo.nasi@gmail.com>
2016-12-16 15:21:08 +0100

    Added 2nd function

--- netscan.py
+++ netscan.py
@@ -40,6 +40,36 @@ def fprinter(f, ip, mac, vend):
             f.write(ip[i] + ',' + k + '\n')
         i=i+1
 
+def checkf(f, ip, mac, vend):
+    know = []
+    entry = []
+    fip=[]
+    fmac=[]
+    fvend=[]
+    for line in f:
+        entry=line.split(',')
+        fip.append(entry[0])
+        fmac.append(entry[1])
+        if len(entry)>2:
+            fvend.append(entry[2])
+    for k in mac:
+        for j in fmac:
+            if k==j:
+                know.append(1)
+            else:
+                know.append(0)
+    return know
+
+def dbadd(f, ip, mac, vend, know):
+    i=0
+    for k in mac:
+        if know[i]==0:
+            if k in vend:
+                f.write(ip[i] + ',' + k + ',' + vend[k] + '\n')
+            else:
+                f.write(ip[i] + ',' + k + '\n')
+    print 'Host added!\n'
+
 # START SCRIPT WITH MENU
 c=0
 while c!=9:
@@ -72,13 +102,47 @@ while c!=9:
         else:
             raise SystemExit
     elif c==2:
+        new=0
         db=glob.glob('database/*.db')
         for net in db:
             print '/%s' % (net)
         netname = raw_input('Insert the Network Name: ')
-        netname = 'database/%s' % (netname)
-        f=open(netname, 'rw')
-        print f
+        netname = 'database/%s.db' % (netname)
+        try:
+            f=open(netname, 'rb')
+            print f
+        except:
+            print 'File opening faile!'
+            print 'Creation of the new DB!'
+            new=1
+            f=open(netname, 'wb+')
+
+        # Network Informations
+        sc_add = raw_input('Address/Netmask(PL): ')
+        nm = nmap.PortScanner()
+        # Call scan function
+        if len(sc_add) == 0:
+            ip, mac, vend = scanner(nm) # Function call for scan default network 192.168.1.0/24
+        else:
+            ip, mac, vend = scanner(nm, sc_add)
+
+        if new==1:
+            fprinter(f,ip,mac,vend) # Calling print to file function
+        else:
+            know = checkf(f,ip,mac,vend) # Calling the checking batabase function
+            i=0
+            for k in mac:
+                if know[i]==0:
+                    if k in vend:
+                        print 'New host: ' + k + ' ---> ' + ip[i] + '(' + vend[k] + ')'
+                    else:
+                        print 'New host: ' + k + ' ---> ' + ip[i]
+                i=i+1
+        m = input('Do you want to add this new host to the db (1): ')
+        if m==1:
+            dbadd(f, ip, mac, vend, know)
+        else:
+            print 'Host deleted!'
     # elif c==3:
     #
     # elif c==4:
